cc3c8b75ee65153a6ebefe960bcf0602ac97b587
Daniel Stephen Ruiz <118859674+EseRuiz@users.noreply.github.com>
2024-01-12 20:31:03 -0500

    Feat: <Creacion listado de lenguajes de programacion>

---
+++ LanguageList/src/Main.java
@@ -0,0 +1,8 @@
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hola");
+        LanguageList lan = new LanguageList();
+        System.out.println(lan.isEmpty());
+    }
+}
