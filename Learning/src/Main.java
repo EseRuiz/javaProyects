493f21e24aaaafc8042fe02ba52111b2c111430b
Daniel Stephen Ruiz <118859674+EseRuiz@users.noreply.github.com>
2024-01-25 09:55:41 -0500

    Feat: <manejo switch case, ilegalargumentexception>

---
+++ Learning/src/Main.java
@@ -0,0 +1,8 @@
+
+public class Main {
+    public static void main(String[] args) {
+        FootballMatchReports player = new FootballMatchReports();
+        String PosPlayer = player.onField(0);
+        System.out.println("PosPlayer = " + PosPlayer);
+    }
+}
