7662b44a69b317294ff0d5af386ab1e3e72700d6
Daniel Stephen Ruiz <118859674+EseRuiz@users.noreply.github.com>
2024-02-19 18:42:14 -0500

    actalizacion

--- Learning/src/Main.java
+++ Learning/src/Main.java
@@ -9,6 +9,10 @@ public class Main {
         System.out.println(cap.randomShipRegistryNumber());
         System.out.println(cap.randomStardate());
         char num = 0b0110 | 0b0101;
-        System.out.println(num);
+        testColors test= new testColors("B");
+        System.out.println(test.GetTestColor());
+        System.out.println(test.getLetterColor());
+        System.out.println(test.getNumberColor());
+
     }
 }
