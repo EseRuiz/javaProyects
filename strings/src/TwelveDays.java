53855a9add4e13bffb217b85249f440fdced5aaf
Daniel Stephen Ruiz <118859674+EseRuiz@users.noreply.github.com>
2024-04-18 18:44:05 -0500

    Feat: <se crea lista con strings de cancion>

---
+++ strings/src/TwelveDays.java
@@ -0,0 +1,42 @@
+class TwelveDays {
+    String[] christmas_song = {
+            "On the first day of Christmas my true love gave to me: a Partridge in a Pear Tree.",
+            "On the second day of Christmas my true love gave to me: two Turtle Doves, and a Partridge in a Pear Tree.",
+            "On the third day of Christmas my true love gave to me: three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.",
+            "On the fourth day of Christmas my true love gave to me: four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.",
+            "On the fifth day of Christmas my true love gave to me: five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.",
+            "On the sixth day of Christmas my true love gave to me: six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.",
+            "On the seventh day of Christmas my true love gave to me: seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.",
+            "On the eighth day of Christmas my true love gave to me: eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.",
+            "On the ninth day of Christmas my true love gave to me: nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.",
+            "On the tenth day of Christmas my true love gave to me: ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.",
+            "On the eleventh day of Christmas my true love gave to me: eleven Pipers Piping, ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.",
+            "On the twelfth day of Christmas my true love gave to me: twelve Drummers Drumming, eleven Pipers Piping, ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.",
+    };
+    String verse(int verseNumber) {
+        return String.valueOf(christmas_song[verseNumber-1]+"\n");
+    }
+
+    String verses(int startVerse, int endVerse) {
+        StringBuilder verseSong = new StringBuilder();
+        for (int i = startVerse-1; i < endVerse; i++) {
+            if (i<endVerse-1){
+                verseSong.append(christmas_song[i]).append("\n\n");
+            }else{verseSong.append(christmas_song[i]).append("\n");}
+
+        }
+        return String.valueOf(verseSong);
+    }
+
+    String sing() {
+        StringBuilder Song = new StringBuilder();
+        for (int i = 0; i < christmas_song.length; i++) {
+            if(i<christmas_song.length-1){
+                Song.append(christmas_song[i]).append("\n\n");
+            }else{
+                Song.append(christmas_song[i]).append("\n");
+            }
+        }
+        return String.valueOf(Song);
+    }
+}
