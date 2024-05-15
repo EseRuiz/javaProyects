e18e36427bfbcb901326ad9b6c5d57912ec5ae4d
Daniel Stephen Ruiz <118859674+EseRuiz@users.noreply.github.com>
2024-05-15 10:21:36 -0500

    Fix:<creacion movimiento de robot con enum, clase posicion x,y>

--- chars/src/Orientation.java
+++ chars/src/Orientation.java
@@ -5,10 +5,12 @@ enum Orientation {
     private Orientation actualOrientation;
 
     public Orientation advance(Orientation actualOrientation) {
-        return values()[(actualOrientation.ordinal() + 1) % values().length];
+        this.actualOrientation = actualOrientation;
+        return values()[(this.actualOrientation.ordinal() + 1) % values().length];
     }
 
     public Orientation retreat(Orientation actualOrientation) {
-        return values()[(actualOrientation.ordinal() - 1 + values().length) % values().length];
+        this.actualOrientation = actualOrientation;
+        return values()[(this.actualOrientation.ordinal() - 1 + values().length) % values().length];
     }
 }
